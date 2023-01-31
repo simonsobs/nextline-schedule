ef36ed80850a20e5a5f1e617f3aa1b09a63eda4b
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-31 16:05:02 -0500

    add query

--- src/nextline_schedule/schema/query.py
+++ src/nextline_schedule/schema/query.py
@@ -1,6 +1,27 @@
 import strawberry
+from strawberry.types import Info
+
+
+def query_auto_mode_state(info: Info) -> str:
+    auto_mode = info.context["auto_mode"]
+    return auto_mode.state
+
+
+@strawberry.type
+class AutoMode:
+    state: str = strawberry.field(resolver=query_auto_mode_state)
+
+
+@strawberry.type
+class Schedule:
+    @strawberry.field
+    def auto_mode(self, info: Info) -> AutoMode:
+        print(info.context)
+        return AutoMode()
 
 
 @strawberry.type
 class Query:
-    pass
+    @strawberry.field
+    def schedule(self, info: Info) -> Schedule:
+        return Schedule()
