e3ddbf3558d40f58660a1f402d7900aae53d08e0
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-31 16:19:13 -0500

    rename

--- src/nextline_schedule/schema/query.py
+++ src/nextline_schedule/schema/query.py
@@ -8,20 +8,20 @@ def query_auto_mode_state(info: Info) -> str:
 
 
 @strawberry.type
-class AutoMode:
+class QueryAutoMode:
     state: str = strawberry.field(resolver=query_auto_mode_state)
 
 
 @strawberry.type
-class Schedule:
+class QuerySchedule:
     @strawberry.field
-    def auto_mode(self, info: Info) -> AutoMode:
+    def auto_mode(self, info: Info) -> QueryAutoMode:
         print(info.context)
-        return AutoMode()
+        return QueryAutoMode()
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def schedule(self, info: Info) -> Schedule:
-        return Schedule()
+    def schedule(self, info: Info) -> QuerySchedule:
+        return QuerySchedule()
