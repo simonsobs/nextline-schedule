a12d9ded1a3d5fd42776cf22b1060e813774f30c
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-01 11:06:01 -0500

    turn a functor into a closure

--- src/nextline_schedule/scheduler.py
+++ src/nextline_schedule/scheduler.py
@@ -2,9 +2,7 @@ from nextline import Nextline
 from .funcs import generate_statement
 
 
-class DummyRequestStatement:
-    def __init__(self, nextline: Nextline):
-        self._nextline = nextline
-
-    async def __call__(self) -> str:
-        return generate_statement(run_no=self._nextline.run_no + 1)
+def DummyRequestStatement(nextline: Nextline):
+    async def f() -> str:
+        return generate_statement(run_no=nextline.run_no + 1)
+    return f
