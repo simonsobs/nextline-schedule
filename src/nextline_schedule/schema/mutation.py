a2793fa43df5975987e294e4c773b2d40bcd7cfd
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-01 12:39:41 -0500

    add mutations

--- src/nextline_schedule/schema/mutation.py
+++ src/nextline_schedule/schema/mutation.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import strawberry
 from strawberry.types import Info
 
@@ -20,11 +22,36 @@ class MutationAutoMode:
     turn_off: bool = strawberry.field(resolver=mutate_turn_off)
 
 
+@strawberry.input
+class MutationSchedulerInput:
+    api_url: Optional[str] = None
+    length_minutes: Optional[int] = None
+    policy: Optional[str] = None
+
+
+@strawberry.type
+class MutationScheduler:
+    @strawberry.mutation
+    def update(self, info: Info, input: MutationSchedulerInput) -> bool:
+        scheduler = info.context["scheduler"]
+        if input.api_url is not None:
+            scheduler._api_url = input.api_url
+        if input.length_minutes is not None:
+            scheduler._length_minutes = input.length_minutes
+        if input.policy is not None:
+            scheduler._policy = input.policy
+        return True
+
+
 @strawberry.type
 class MutationSchedule:
     @strawberry.field
     def auto_mode(self, info: Info) -> MutationAutoMode:
         return MutationAutoMode()
+
+    @strawberry.field
+    def scheduler(self, info: Info) -> MutationScheduler:
+        return MutationScheduler()
 
 
 @strawberry.type
