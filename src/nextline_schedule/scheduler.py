28170b1e5b874ca94dd1535d235708e1a9430d9f
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-01 11:48:25 -0500

    make DummyRequestStatement a subclass of RequestStatement

--- src/nextline_schedule/scheduler.py
+++ src/nextline_schedule/scheduler.py
@@ -2,7 +2,34 @@ import random
 from datetime import datetime, timedelta
 
 import httpx
-from nextline import Nextline
+
+# https://github.com/simonsobs/so-scheduler/blob/master/readme.md#schedule-api
+API_URL = 'https://scheduler-uobd.onrender.com/api/v1/schedule/'
+
+
+class RequestStatement:
+    def __init__(self):
+        self._api_url = API_URL
+        self._length_minutes = 1
+        self._policy = 'dummy'
+
+    async def __call__(self) -> str:
+        start_time = datetime.utcnow()
+        duration = timedelta(minutes=self._length_minutes)
+        end_time = start_time + duration
+        start_time_str = start_time.strftime('%Y-%m-%d %H:%M')
+        end_time_str = end_time.strftime('%Y-%m-%d %H:%M')
+        data = {"t0": start_time_str, "t1": end_time_str, "policy": self._policy}
+        print(data)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(API_URL, json=data)
+        print(response.json())
+        return response.json()['commands']
+
+
+class DummyRequestStatement(RequestStatement):
+    async def __call__(self) -> str:
+        return generate_dummy_statement()
 
 
 def generate_dummy_statement() -> str:
@@ -18,32 +45,3 @@ def generate_dummy_statement() -> str:
         )
     )
     return s
-
-
-class DummyRequestStatement:
-    async def __call__(self) -> str:
-        return generate_dummy_statement()
-
-
-# https://github.com/simonsobs/so-scheduler/blob/master/readme.md#schedule-api
-API_URL = 'https://scheduler-uobd.onrender.com/api/v1/schedule/'
-
-
-class RequestStatement:
-    def __init__(self, nextline: Nextline):
-        self._nextline = nextline
-        self._api_url = API_URL
-        self._length_minutes = 1
-
-    async def __call__(self) -> str:
-        start_time = datetime.utcnow()
-        duration = timedelta(minutes=self._length_minutes)
-        end_time = start_time + duration
-        start_time_str = start_time.strftime('%Y-%m-%d %H:%M')
-        end_time_str = end_time.strftime('%Y-%m-%d %H:%M')
-        data = {"t0": start_time_str, "t1": end_time_str, "policy": "dummy"}
-        print(data)
-        async with httpx.AsyncClient() as client:
-            response = await client.post(API_URL, json=data)
-        print(response.json())
-        return response.json()['commands']
