9fb7271075fd7e5e13b6b3e4dea074f499ca1119
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-01 11:41:28 -0500

    move generate_statement() to scheduler.py

--- src/nextline_schedule/scheduler.py
+++ src/nextline_schedule/scheduler.py
@@ -1,7 +1,23 @@
+import random
 from datetime import datetime, timedelta
+
 import httpx
 from nextline import Nextline
-from .funcs import generate_statement
+
+
+def generate_statement() -> str:
+    s = '\n'.join(
+        f'time.sleep({random.uniform(1, 3):.2})' for _ in range(random.randint(5, 10))
+    )
+    s = '\n'.join(
+        (
+            f'# {datetime.utcnow().isoformat()}',
+            'import time',
+            '',
+            s,
+        )
+    )
+    return s
 
 
 def DummyRequestStatement():
