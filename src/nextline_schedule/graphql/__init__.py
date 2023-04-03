64deb2abbea17cdf3ef8f48f68ba9d60ae11ce87
Tai Sakuma <tai.sakuma@gmail.com>
2023-04-03 13:36:20 -0400

    add GraphQL query strings

---
+++ src/nextline_schedule/graphql/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+pwd = Path(__file__).resolve().parent
+
+sub = pwd / 'mutations'
+MUTATE_AUTO_MODE_TURN_ON = (sub / 'AutoModeTurnOn.gql').read_text()
+MUTATE_AUTO_MODE_TURN_OFF = (sub / 'AutoModeTurnOff.gql').read_text()
+
+
+sub = pwd / 'queries'
+QUERY_AUTO_MODE = (sub / 'AutoMode.gql').read_text()
+QUERY_SCHEDULER = (sub / 'Scheduler.gql').read_text()
