610e73f6e80c8b8dec5bce820c26db4ea9e7c669
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-19 11:28:03 -0400

    replace TypeAlias and Union

--- src/nextline_schedule/types.py
+++ src/nextline_schedule/types.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 from types import CodeType
-from typing import Any, Callable, Union
+from typing import Any, Callable
 
-from typing_extensions import TypeAlias
-
-Statement: TypeAlias = Union[str, Path, CodeType, Callable[[], Any]]
+Statement = str | Path | CodeType | Callable[[], Any]
