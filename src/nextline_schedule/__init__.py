e560a72e042c7815008238f6b7cff70357ee3f2c
Tai Sakuma <tai.sakuma@gmail.com>
2024-03-21 14:11:01 -0400

    Add version string to the package

--- src/nextline_schedule/__init__.py
+++ src/nextline_schedule/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['Plugin']
+__all__ = ['__version__', 'Plugin']
 
 
+from .__about__ import __version__
 from .plugin import Plugin
---
+++ tests/test_version.py
@@ -0,0 +1,6 @@
+import nextline_schedule
+
+
+def test_version():
+    '''Confirm that the version string is attached to the module'''
+    nextline_schedule.__version__
