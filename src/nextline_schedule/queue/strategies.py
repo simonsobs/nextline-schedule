dc61c1dea85a0f7ec2bec488eb3455fff60559aa
Tai Sakuma <tai.sakuma@gmail.com>
2024-04-30 14:22:58 -0400

    Add Hypothesis strategies

---
+++ src/nextline_schedule/queue/strategies.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from hypothesis import strategies as st
+
+from nextline_schedule.utils.strategies import st_datetimes, st_python_scripts
+
+from .queue import Queue, QueueItem
+
+
+def st_queue_item() -> st.SearchStrategy[QueueItem]:
+    return st.builds(
+        QueueItem,
+        name=st.text(),
+        created_at=st_datetimes(),
+        script=st_python_scripts(),
+    )
+
+
+def st_queue_item_list(
+    min_size: int = 0, max_size: Optional[int] = None
+) -> st.SearchStrategy[list[QueueItem]]:
+    return st.lists(st_queue_item(), min_size=min_size, max_size=max_size)
+
+
+def st_queue(
+    min_items_size: int = 0, max_items_size: int = 5
+) -> st.SearchStrategy[Queue]:
+    return st.builds(
+        Queue,
+        items=st_queue_item_list(min_size=min_items_size, max_size=max_items_size),
+    )
