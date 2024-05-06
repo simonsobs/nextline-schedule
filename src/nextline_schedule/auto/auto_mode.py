155de401d7a7084306c29931f29e846da1643ea1
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-06 11:57:01 -0400

    Add on_from_changed() to AutoMode and PullFrom

--- src/nextline_schedule/auto/auto_mode.py
+++ src/nextline_schedule/auto/auto_mode.py
@@ -1,5 +1,5 @@
 import dataclasses
-from collections.abc import AsyncIterator
+from collections.abc import AsyncIterator, Awaitable, Callable
 
 from nextline import Nextline
 
@@ -11,7 +11,9 @@ from .types import PullFunc
 
 class AutoMode:
     def __init__(self, nextline: Nextline, scheduler: PullFunc, queue: PullFunc):
-        self._pull_from = PullFrom(scheduler=scheduler, queue=queue)
+        self._pull_from = PullFrom(
+            scheduler=scheduler, queue=queue, on_from_changed=self.on_from_changed
+        )
         callback = Callback(
             nextline=nextline,
             pull_func=self._pull_from,
@@ -42,6 +44,9 @@ class AutoMode:
     async def on_state_changed(self, state: str) -> None:
         pass
 
+    async def on_from_changed(self, name: str) -> None:
+        pass
+
 
 @dataclasses.dataclass
 class PullFromItem:
@@ -50,10 +55,30 @@ class PullFromItem:
 
 
 class PullFrom:
-    def __init__(self, scheduler: PullFunc, queue: PullFunc):
+    def __init__(
+        self,
+        scheduler: PullFunc,
+        queue: PullFunc,
+        on_from_changed: Callable[[str], Awaitable[None]],
+    ):
         self._scheduler = PullFromItem(name='scheduler', pull=scheduler)
         self._queue = PullFromItem(name='queue', pull=queue)
+        self._on_from_changed = on_from_changed
+        self._map = {
+            self._scheduler.name: self._scheduler,
+            self._queue.name: self._queue,
+        }
         self._current = self._scheduler
 
     async def __call__(self) -> Statement:
         return await self._current.pull()
+
+    async def change_from(self, name: str) -> None:
+        old = self._current.name
+        self._current = self._map[name]
+        if old != self._current.name:
+            await self._on_from_changed(self._current.name)
+
+    @property
+    def current(self) -> str:
+        return self._current.name
