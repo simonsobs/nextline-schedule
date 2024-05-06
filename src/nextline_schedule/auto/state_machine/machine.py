e5af64c61923a0f9d9daf7ec8d93f9f6ed1a45ca
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-06 10:21:22 -0400

    Update types in machine.py

--- src/nextline_schedule/auto/state_machine/machine.py
+++ src/nextline_schedule/auto/state_machine/machine.py
@@ -1,7 +1,7 @@
 import asyncio
-from collections.abc import AsyncIterator
+from collections.abc import AsyncIterator, Callable
 from logging import getLogger
-from typing import Protocol
+from typing import Any, Protocol
 
 from nextline.utils import pubsub
 
@@ -27,9 +27,15 @@ class AutoModeStateMachine:
         self._tasks = set[asyncio.Task]()
         self._pubsub_state = pubsub.PubSubItem[str]()
         self._logger = getLogger(__name__)
+
         machine = build_state_machine(model=self)
-        machine.after_state_change = self.after_state_change.__name__  # type: ignore
+        machine.after_state_change = [self.after_state_change.__name__]
+
         self.state: str  # attached by machine
+        self.start: Callable[..., Any]  # attached by machine
+
+        assert isinstance(self.state, str)
+        assert callable(self.start)
 
     def subscribe_state(self) -> AsyncIterator[str]:
         return self._pubsub_state.subscribe()
@@ -53,7 +59,7 @@ class AutoModeStateMachine:
 
     async def after_state_change(self) -> None:
         await self._collect_tasks()
-        await self._pubsub_state.publish(self.state)  # type: ignore
+        await self._pubsub_state.publish(self.state)
 
     async def cancel_task(self) -> None:
         self._task.cancel()
@@ -76,7 +82,7 @@ class AutoModeStateMachine:
         await self._pubsub_state.close()
 
     async def __aenter__(self) -> 'AutoModeStateMachine':
-        await self.start()  # type: ignore
+        await self.start()
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback) -> None:
