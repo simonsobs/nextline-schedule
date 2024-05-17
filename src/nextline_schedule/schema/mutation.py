2ed10a31028e9cebc257bab669bd2b46db1897b5
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-17 09:58:11 -0400

    Move MutationScheduleScheduler and QueryScheduleScheduler to a new sub package

--- src/nextline_schedule/schema/mutation.py
+++ src/nextline_schedule/schema/mutation.py
@@ -1,49 +1,10 @@
-from typing import Optional
-
 import strawberry
-from nextline import Nextline
 from strawberry.types import Info
-
-from nextline_schedule.scheduler import Scheduler
 
 from .auto import MutationScheduleAutoMode
 from .queue import MutationScheduleQueue
-
-
-@strawberry.input
-class MutationSchedulerInput:
-    api_url: Optional[str] = None
-    length_minutes: Optional[int] = None
-    policy: Optional[str] = None
-
-
-async def mutate_load_script(info: Info) -> bool:
-    nextline = info.context["nextline"]
-    assert isinstance(nextline, Nextline)
-    scheduler = info.context['schedule']['scheduler']
-    assert callable(scheduler)
-    statement = await scheduler()
-    assert isinstance(statement, str)
-    await nextline.reset(statement=statement)
-    return True
-
-
-def mutate_update(info: Info, input: MutationSchedulerInput) -> bool:
-    scheduler = info.context['schedule']['scheduler']
-    assert isinstance(scheduler, Scheduler)
-    if input.api_url is not None:
-        scheduler._api_url = input.api_url
-    if input.length_minutes is not None:
-        scheduler._length_minutes = input.length_minutes
-    if input.policy is not None:
-        scheduler._policy = input.policy
-    return True
-
-
-@strawberry.type
-class MutationScheduleScheduler:
-    update: bool = strawberry.mutation(resolver=mutate_update)
-    load_script: bool = strawberry.mutation(resolver=mutate_load_script)
+from .scheduler import MutationScheduleScheduler
+from .scheduler.mutation import mutate_load_script
 
 
 @strawberry.type
@@ -56,14 +17,14 @@ class MutationSchedule:
     def scheduler(self) -> MutationScheduleScheduler:
         return MutationScheduleScheduler()
 
+    @strawberry.field
+    def queue(self) -> MutationScheduleQueue:
+        return MutationScheduleQueue()
+
     @strawberry.mutation
     async def load_script(self, info: Info) -> bool:
         '''TODO: Deprecated. Moved to MutationScheduleScheduler.'''
         return await mutate_load_script(info)
-
-    @strawberry.field
-    def queue(self) -> MutationScheduleQueue:
-        return MutationScheduleQueue()
 
 
 @strawberry.type
--- src/nextline_schedule/schema/query.py
+++ src/nextline_schedule/schema/query.py
@@ -1,36 +1,10 @@
 import strawberry
-from strawberry.types import Info
 
 from nextline_schedule import __version__
-from nextline_schedule.scheduler import Scheduler
 
 from .auto import QueryScheduleAutoMode
 from .queue import QueryScheduleQueue
-
-
-def query_scheduler_api_url(info: Info) -> str:
-    scheduler = info.context['schedule']['scheduler']
-    assert isinstance(scheduler, Scheduler)
-    return scheduler._api_url
-
-
-def query_scheduler_length_minutes(info: Info) -> int:
-    scheduler = info.context['schedule']['scheduler']
-    assert isinstance(scheduler, Scheduler)
-    return scheduler._length_minutes
-
-
-def query_scheduler_policy(info: Info) -> str:
-    scheduler = info.context['schedule']['scheduler']
-    assert isinstance(scheduler, Scheduler)
-    return scheduler._policy
-
-
-@strawberry.type
-class QueryScheduleScheduler:
-    api_url: str = strawberry.field(resolver=query_scheduler_api_url)
-    length_minutes: int = strawberry.field(resolver=query_scheduler_length_minutes)
-    policy: str = strawberry.field(resolver=query_scheduler_policy)
+from .scheduler import QueryScheduleScheduler
 
 
 @strawberry.type
