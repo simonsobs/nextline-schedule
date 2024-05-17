898c9e769a38b4c3d4364dc163e66a16127277c1
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-17 08:24:33 -0400

    Check syntax for GraphQL queries while reading them from files

--- src/nextline_schedule/graphql/__init__.py
+++ src/nextline_schedule/graphql/__init__.py
@@ -1,18 +1,30 @@
+from os import PathLike
 from pathlib import Path
+
+from graphql import parse, print_ast
+
+
+def read_gql(path: PathLike | str) -> str:
+    '''Load a GraphQL query from a file while checking its syntax.'''
+
+    text = Path(path).read_text()
+    parsed = parse(text)
+    reformatted = print_ast(parsed)
+    return reformatted
+
 
 pwd = Path(__file__).resolve().parent
 
 sub = pwd / 'mutations'
-MUTATE_AUTO_MODE_TURN_ON = (sub / 'AutoModeTurnOn.gql').read_text()
-MUTATE_AUTO_MODE_TURN_OFF = (sub / 'AutoModeTurnOff.gql').read_text()
-MUTATE_QUEUE_PUSH = (sub / 'QueuePush.gql').read_text()
-
+MUTATE_AUTO_MODE_TURN_ON = read_gql(sub / 'AutoModeTurnOn.gql')
+MUTATE_AUTO_MODE_TURN_OFF = read_gql(sub / 'AutoModeTurnOff.gql')
+MUTATE_QUEUE_PUSH = read_gql(sub / 'QueuePush.gql')
 
 sub = pwd / 'queries'
-QUERY_AUTO_MODE = (sub / 'AutoMode.gql').read_text()
-QUERY_SCHEDULER = (sub / 'Scheduler.gql').read_text()
-QUERY_VERSION = (sub / 'Version.gql').read_text()
-QUERY_SCHEDULE_QUEUE_ITEMS = (sub / 'QueueItems.gql').read_text()
+QUERY_AUTO_MODE = read_gql(sub / 'AutoMode.gql')
+QUERY_SCHEDULER = read_gql(sub / 'Scheduler.gql')
+QUERY_VERSION = read_gql(sub / 'Version.gql')
+QUERY_SCHEDULE_QUEUE_ITEMS = read_gql(sub / 'QueueItems.gql')
 
 sub = pwd / 'subscriptions'
-SUBSCRIBE_AUTO_MODE_STATE = (sub / 'ScheduleAutoModeState.gql').read_text()
+SUBSCRIBE_AUTO_MODE_STATE = read_gql(sub / 'ScheduleAutoModeState.gql')
