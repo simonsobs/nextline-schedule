c868e31be528efb9d47c6ac990ed4787544a220c
Tai Sakuma <tai.sakuma@gmail.com>
2024-06-25 13:41:10 -0400

    Add `pragma: no cover`

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,6 @@ import os
 
 from hypothesis import settings
 
-if os.getenv('GITHUB_ACTIONS') == 'true':
+if os.getenv('GITHUB_ACTIONS') == 'true':  # pragma: no cover
     settings.register_profile('ci', deadline=None)
     settings.load_profile('ci')
