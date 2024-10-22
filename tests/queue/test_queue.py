3fa436a3d2286caeb55feec49e5c45b49946d8d5
Tai Sakuma <tai.sakuma@gmail.com>
2024-10-22 14:01:31 -0400

    Rewrite `test_queue.py` as a stateful test

--- tests/queue/test_queue.py
+++ tests/queue/test_queue.py
@@ -1,68 +1,97 @@
 import asyncio
+from typing import Any
 
-from hypothesis import given
+import pytest
+from hypothesis import given, settings
 from hypothesis import strategies as st
 
 from nextline_schedule.queue import QueueEmpty, QueueItem
 from nextline_schedule.queue.strategies import st_push_arg, st_queue
 
 
-@given(st.data())
-async def test_queue(data: st.DataObject) -> None:
-    queue = data.draw(st_queue())
+class StatefulTest:
+    def __init__(self, data: st.DataObject) -> None:
+        self._draw = data.draw
+        self._queue = data.draw(st_queue())
+        self._expected = list[list[QueueItem]]()
 
-    async def subscribe() -> list[list[QueueItem]]:
-        return [i async for i in queue.subscribe()]
+    async def _subscribe(self) -> list[list[QueueItem]]:
+        return [i async for i in self._queue.subscribe()]
 
-    task = asyncio.create_task(subscribe())
-    await asyncio.sleep(0)
+    def _on_expected_publish(self) -> None:
+        self._expected.append(list(self._queue.items))
 
-    METHODS = ['call', 'pop', 'push', 'remove', 'break']
-    methods = data.draw(st.lists(st.sampled_from(METHODS), min_size=0, max_size=10))
+    async def call(self) -> None:
+        empty = not self._queue
+        if empty:
+            with pytest.raises(QueueEmpty):
+                await self._queue()
+        else:
+            script = await self._queue()
+            assert isinstance(script, str)
+            self._on_expected_publish()
 
-    async with queue:
-        expected = [queue.items]
+    async def pop(self) -> None:
+        empty = not self._queue
+        if empty:
+            assert await self._queue.pop() is None
+        else:
+            item = await self._queue.pop()
+            assert item is not None
+            self._on_expected_publish()
+
+    async def push(self) -> None:
+        arg = self._draw(st_push_arg())
+        await self._queue.push(arg)
+        self._on_expected_publish()
+
+    async def remove(self) -> None:
+        ids = [i.id for i in self._queue.items]
+        ids.append(self._draw(st.integers(min_value=0)))
+        id = self._draw(st.sampled_from(ids))
+        success = await self._queue.remove(id)
+        if success:
+            self._on_expected_publish()
+
+    async def __aenter__(self) -> 'StatefulTest':
+        self._task = asyncio.create_task(self._subscribe())
+        await asyncio.sleep(0)
+        await self._queue.__aenter__()
+        self._on_expected_publish()
+        return self
+
+    async def __aexit__(self, *args: Any, **kwargs: Any) -> None:
+        await self._queue.__aexit__(*args, **kwargs)
+        await asyncio.sleep(0)
+        actual = await self._task
+        assert self._expected == actual
+
+
+@settings(max_examples=500)
+@given(data=st.data())
+async def test_property(data: st.DataObject) -> None:
+    test = StatefulTest(data)
+
+    METHODS = [
+        test.call,
+        test.pop,
+        test.push,
+        test.remove,
+    ]
+
+    methods = data.draw(st.lists(st.sampled_from(METHODS)))
+
+    async with test:
         for method in methods:
             await asyncio.sleep(0)
-            empty = not queue
-            match method, empty:
-                case 'call', True:
-                    try:
-                        await queue()
-                    except QueueEmpty:
-                        continue
-                case 'call', False:
-                    script = await queue()
-                    assert isinstance(script, str)
-                case 'pop', True:
-                    assert await queue.pop() is None
-                    continue
-                case 'pop', False:
-                    item = await queue.pop()
-                    assert item is not None
-                case 'push', _:
-                    arg = data.draw(st_push_arg())
-                    await queue.push(arg)
-                case 'remove', _:
-                    ids = [i.id for i in queue.items]
-                    ids.append(data.draw(st.integers(min_value=0)))
-                    id = data.draw(st.sampled_from(ids))
-                    success = await queue.remove(id)
-                    if not success:
-                        continue
-                case 'break', _:
-                    break
-                case _:  # pragma: no cover
-                    raise ValueError(f'Invalid method: {method!r}')
-            expected.append(list(queue.items))
+            await method()
 
-    await asyncio.sleep(0)
+    await cancel_extra_tasks()
 
-    actual = await task
-    assert actual == expected
 
-    # Cancel extra tasks. For unknown reasons, the task created by
-    # `queue.subscribe()` in the `subscribe` function is sometimes still running.
+async def cancel_extra_tasks() -> None:
+    # For unknown reasons, the task created by `queue.subscribe()` in the
+    # `_subscribe` method is sometimes still running.
     extra_tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
     for t in extra_tasks:  # pragma: no cover
         t.cancel()
