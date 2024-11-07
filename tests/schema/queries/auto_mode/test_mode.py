56956884e25ec0190d5fe13493d84ba622b046cc
Tai Sakuma <tai.sakuma@gmail.com>
2024-11-07 12:01:49 -0500

    Update tests

--- tests/schema/queries/auto_mode/test_mode.py
+++ tests/schema/queries/auto_mode/test_mode.py
@@ -1,21 +1,16 @@
-from strawberry.types import ExecutionResult
+from unittest.mock import Mock
 
-from nextline import Nextline
 from nextline_schedule.auto import AutoMode
 from nextline_schedule.graphql import QUERY_AUTO_MODE_MODE
-from tests.auto.funcs import STATEMENT_TEMPLATE, pull_func_factory
 from tests.schema.conftest import Schema
 
 
 async def test_schema(schema: Schema) -> None:
-    statement = STATEMENT_TEMPLATE.format(name='init', count=1)
-    nextline = Nextline(statement=statement)
-    scheduler = pull_func_factory('schedule')
-    queue = pull_func_factory('queue')
-    auto_mode = AutoMode(nextline=nextline, scheduler=scheduler, queue=queue)
+    auto_mode = Mock(spec=AutoMode)
+    mode = 'off'
+    auto_mode.mode = mode
     context_auto_mode = {'auto_mode': auto_mode}
     context = {'schedule': context_auto_mode}
     resp = await schema.execute(QUERY_AUTO_MODE_MODE, context_value=context)
-    assert isinstance(resp, ExecutionResult)
     assert resp.data
-    assert resp.data['schedule']['autoMode']['mode'] == auto_mode.mode
+    assert resp.data['schedule']['autoMode']['mode'] == mode
