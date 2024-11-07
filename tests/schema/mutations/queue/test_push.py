7ad9ef221b1b0810c13c6a7f93b90f6cbd297b9b
Tai Sakuma <tai.sakuma@gmail.com>
2024-11-07 14:19:14 -0500

    Rewrite `test_push.py` with `Mock`

--- tests/schema/mutations/queue/test_push.py
+++ tests/schema/mutations/queue/test_push.py
@@ -1,12 +1,14 @@
-from typing import Any, TypedDict
+from typing import TypedDict
+from unittest.mock import Mock
 
 from deepdiff import DeepDiff
 from hypothesis import Phase, given, settings
 from hypothesis import strategies as st
-from strawberry.types import ExecutionResult
 
 from nextline_schedule.graphql import MUTATE_QUEUE_PUSH
-from nextline_schedule.queue.strategies import Queue, st_push_arg, st_queue
+from nextline_schedule.queue import PushArg, Queue, QueueItem
+from nextline_schedule.queue.strategies import st_push_arg
+from nextline_test_utils.strategies import st_datetimes, st_graphql_ints
 from tests.schema.conftest import Schema
 
 
@@ -22,67 +24,40 @@ def st_input(draw: st.DrawFn) -> Input:
 
 
 @settings(phases=(Phase.generate,))  # To avoid shrinking
-@given(queue=st_queue(), input=st_input())
-async def test_schema(queue: Queue, input: Input, schema: Schema) -> None:
-    '''
+@given(d=st.data())
+async def test_schema(d: st.DataObject, schema: Schema) -> None:
+    input = d.draw(st_input())
+    item = QueueItem(
+        **input,
+        id=d.draw(st_graphql_ints(min_value=1)),
+        created_at=d.draw(st_datetimes()),
+    )
 
-    NOTE: Only the generate phase is included in the settings. This is because
-    the shrink phase doesn't make much difference and only takes much longer
-    time to run. (https://hypothesis.readthedocs.io/en/latest/settings.html)
-    '''
-    async with queue:
-        len_ini = len(queue.items)
+    queue = Mock(spec=Queue)
+    queue.push.return_value = item
 
-        context_values = {'schedule': {'queue': queue}}
-        variable_values = {'input': input}
+    context_values = {'schedule': {'queue': queue}}
+    variable_values = {'input': input}
 
-        resp = await schema.execute(
-            MUTATE_QUEUE_PUSH,
-            context_value=context_values,
-            variable_values=variable_values,
-        )
-        assert isinstance(resp, ExecutionResult)
-        assert (data := resp.data)
-        assert_data(data, input)
-        assert_queue_item(queue, data, input, len_ini)
+    resp = await schema.execute(
+        MUTATE_QUEUE_PUSH,
+        context_value=context_values,
+        variable_values=variable_values,
+    )
 
-
-def assert_data(data: dict[str, Any], input: Input) -> None:
-    ref_resp_data = {
+    assert (data := resp.data)
+    expected_data = {
         'schedule': {
             'queue': {
                 'push': {
-                    'name': input['name'],
-                    'script': input['script'],
+                    'name': item.name,
+                    'script': item.script,
+                    'id': item.id,
+                    'createdAt': item.created_at.isoformat(),
                 }
             }
         }
     }
+    assert DeepDiff(data, expected_data) == {}
 
-    # The 'id' and 'createdAt' are excluded from the reference response data
-    # because their values are generated and not part of the input data.
-    expected_diff = {
-        'dictionary_item_removed': [
-            "root['schedule']['queue']['push']['id']",
-            "root['schedule']['queue']['push']['createdAt']",
-        ]
-    }
-
-    diff = DeepDiff(data, ref_resp_data)
-    assert diff == expected_diff
-
-
-def assert_queue_item(
-    queue: Queue, data: dict[str, Any], input: Input, len_ini: int
-) -> None:
-    assert len(queue.items) == len_ini + 1
-
-    id = data['schedule']['queue']['push']['id']
-    created_at = data['schedule']['queue']['push']['createdAt']
-
-    last_item = queue.items[-1]
-
-    assert last_item.id == id
-    assert last_item.name == input['name']
-    assert last_item.script == input['script']
-    assert last_item.created_at.isoformat() == created_at
+    queue.push.assert_called_once_with(PushArg(**input))
